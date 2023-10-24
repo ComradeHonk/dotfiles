29ec754c6e598b42acfc3209500bda0f5b7d70f3
ComradeHonk <bozydar427050@gmail.com>
2023-10-24 17:50:31 +0200

    Initial commit

---
+++ fish/functions/ll.fish
@@ -0,0 +1,4 @@
+function ll --wraps=ls --wraps='eza -al --icons' --description 'alias ll=eza -al --icons'
+  eza -al --icons $argv
+        
+end
---
+++ fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza -a --icons' --description 'alias ls=eza -a --icons'
+  eza -a --icons $argv
+        
+end
